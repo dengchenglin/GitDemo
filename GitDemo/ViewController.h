26b000a8d89431a1fe6c8984da801af7886c3625
peikua <peikua@huangchengdudeiMac.local>
2016-12-06 14:11:22 +0800

    修改

--- GitDemo/ViewController.h
+++ GitDemo/ViewController.h
@@ -3,8 +3,7 @@
 //  GitDemo
 //
 //  Created by peikua on 16/11/28.
-//  Copyright © 2016年 peikua. All rights reserved.
-//
+//  Copyright © 2016年 peikua. All rights reserved.  vvvvvvvcchttp://ios.jobbole.com/90957/vvvvvvvccvnnnn                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   //
 
 #import <UIKit/UIKit.h>
 
